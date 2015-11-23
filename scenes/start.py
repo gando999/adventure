27ddabfa4f7c4831eed29c97204b7d0e5d26c313
Helen Williams <helen.williams@fanduel.com>
2015-11-23 14:24:17 +0000

    basic scene for development

---
+++ scenes/start.py
@@ -0,0 +1,7 @@
+from adventure.base import Scene
+
+scene = Scene()
+
+scene.description = "In the begining, we find ourselves faced with several choices"
+
+scene.choices = {'go left': 'scene002', 'go right': 'scene003', 'go home': 'terminal'}
